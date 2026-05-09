97ee9ac51a45d1b7f99b580ad8062a21c5604ef3
MGNFAH <134407420+MGNFAH@users.noreply.github.com>
2026-05-09 21:44:27 +0200

    adding db.js for the backend dev

--- server/server.js
+++ server/server.js
@@ -2,8 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const { connectDB, sequelize } = require("./config/db");
+  require("./models/Item");
 
-const app = express();
+ 
+// Connect to database
+ connectDB().then(() => sequelize.sync());
+ const app = express();
+
 
 // Middleware
 app.use(helmet());
@@ -15,7 +21,7 @@ app.get("/api/test", (req, res) => {
   res.json({ message: "Backend is working!" });
 });
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
